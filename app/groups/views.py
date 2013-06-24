0441ce08f5406f46cd6b0fe4605e4d55981a6ff6
Remi Gagne <remigagne@gmail.com>
2013-06-24 12:40:14 -0700

    Simple, ugly group creation UI.
    Simple UI to let users create a group.  Choose a name, url (short name),
    description and rules, and you're done.  As usual, it's ugly but
    functional.

--- app/groups/views.py
+++ app/groups/views.py
@@ -1,5 +1,6 @@
+import re
 from flask import *
-from app import app, db, breakpoint, strip
+from app import db, oid, breakpoint, strip
 from app.models import *
 
 from app.users.decorators import requires_login
@@ -14,7 +15,37 @@ def root():
 def group(groupURL):
     group = Group.query.filter_by(url_name = groupURL).first()
     return render_template('groups/group.html', group = group)
+    
+pattern = re.compile('[^A-Za-z0-9_]+')
+@mod.route('/create_group/', methods = ['GET', 'POST'])
+@requires_login
+def create_group():
+    if request.method == 'POST':
 
+        name = strip(request.form.get('name'))
+        url_name = pattern.sub('', request.form.get('url_name'))
+        desc = request.form.get('description')
+        rules = request.form.get('rules')
+
+        if Group.query.filter_by(name = name).count() > 0:
+            flash(u'A Group with the name "%s" already exists - choose another.' % (name))
+            return render_template('groups/create_group.html', name = name, url_name = url_name, description = desc, rules = rules)
+
+        if Group.query.filter_by(url_name = url_name).count() > 0:
+            flash(u'A Group with the short name "%s" already exists - choose another.' % (url_name))
+            return render_template('groups/create_group.html', name = name, url_name = url_name, description = desc, rules = rules)
+
+        group = Group(name, url_name, desc, rules)
+        group.members.append(g.user)
+        db.session.commit()
+        return redirect(url_for('groups.group', groupURL = url_name))
+    return render_template('groups/create_group.html')
+
+@mod.route('/delete_group/<groupURL>', methods = ['GET', 'POST'])
+@requires_login
+def delete_group(groupURL):  # NYI
+    return redirect(url_for('groups.group', groupURL = groupURL))
+    
 @mod.route('/<groupURL>/add_photos/', methods = ['GET', 'POST'])
 @requires_login
 def add_photos(groupURL):
